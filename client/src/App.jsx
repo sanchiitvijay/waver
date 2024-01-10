d5aee4ed6ad6c9df541c88544c40b011432644ac
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-01-11 00:37:16 +0530

    11-01-23

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
-import HomePage from 'scenes/homePage/homePage'
-import LoginPage from 'scenes/loginPage/loginPage'
-import ProfilePage from 'scenes/profilePage/profilePage'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './scenes/homePage/homePage';
+import LoginPage from './scenes/loginPage/loginPage';
+import ProfilePage from './scenes/profilePage/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme.js';
+// import { ThemeProvider } from '@emotion/react';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className='app'>
-      app
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+      <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+      </ThemeProvider>
       </BrowserRouter>
     </div>
   )
